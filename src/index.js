bf55ec8443c9e25d103213c2c4ed3d9bd7122c50
Sarabouch <sara93bo@hotmail.com>
2018-05-28 14:52:03 +0200

    css and devServer added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// we can import functions here from different files
+import { changeColor } from "./homepage.js";
+// we link our css file here instead of html
+import "./styles/main.css";
+
+
+
 function Test(){
     console.log("This is my first webpack");
 }
 Test();
-
-import { changeColor } from "./homepage.js";
 
 changeColor("h1", "blue");
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 const path = require("path");
+const webpack = require('webpack');
 
-module.exports ={
+
+module.exports = {
     // The main file for our js
-    entry:"./src/index.js",
+    entry: "./src/index.js",
     // The output files
-    output:{
+    output: {
         // where we want to put our output files
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist/assets"),
         // The name for our main files
         filename: "bundle.js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist',
+        hot :true
+    },
+    plugins:  [new webpack.HotModuleReplacementPlugin()]
 };
 
-
 // yarn run build "we use this command in the terminal with every change we make"
-
